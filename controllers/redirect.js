a4e477582299f7763c8f1b74b1740ebbb4e0e4a4
Hemant-kkr <kumarhemant122156@gmail.com>
2025-09-06 16:41:00 +0530

    Redirect the to og url and capture it if url is generated by user

--- controllers/redirect.js
+++ controllers/redirect.js
@@ -3,29 +3,29 @@ const URL = require('../models/url')
 const CLICKS = require('../models/clicks')
 const geoip = require("geoip-lite");
 
- async function  redirectLogic(req, res) {
+async function redirectLogic(req, res) {
     let userAgent = req.headers['user-agent'];
     const parser = new UAParser();
     parser.setUA(userAgent);
     let result = parser.getResult();
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-      if (ip.includes(",")) ip = ip.split(",")[0].trim();
+    if (ip.includes(",")) ip = ip.split(",")[0].trim();
 
     const location = geoip.lookup(ip);
     let shortid = req.params.Shortid;
     const url = await URL.findOne({ shortId: shortid });
     if (!url) return res.json({ error: 'not found' });
     else {
-         if (req.session.userId && req.session.email)
-         {
-        await CLICKS.create({
-            url:url._id,
-            country:location["country"]||"other",
-            city:location["city"]||"other",
-            Browser:result.browser['name']|| "other",
-            device: result.device['type']||'other'
-          })
-         }
+        if (url.createdBy) {
+          const test=  await CLICKS.create({
+                url: url._id,
+                country: location["country"] ||'other',
+                city: location["city"]  ||'other',
+                Browser: result.browser['name'] ||'other',
+                device: result.device['type'] ||'other'
+            })
+          
+        }
         res.redirect(url.redirectURL);
     }
 
