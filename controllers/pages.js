8ce538bfa4c03a9a0f6ee53831bb2073bc2baf66
Hemant-kkr <kumarhemant122156@gmail.com>
2025-09-06 11:54:24 +0530

    Working on Analytics Page

--- controllers/pages.js
+++ controllers/pages.js
@@ -29,43 +29,45 @@ async function analyticsPage(req, res) {
     const userId = req.session.userId;
     const userEmail = req.session.email;
     const userrole = req.session.role;
-
-    const url = await URL.find({ shortId: shortid });
     const UserAllUrls = await URL.find({ createdBy: userId });
+
+    const url = await URL.findOne({ shortId: shortid });
+    if (!url) {
+        return res.status(404).send("URL not found");
+    }
+
     const clicks = await CLICKS.find({ url: url._id });
 
     const countryStats = await CLICKS.aggregate([
+        { $match: { url: url._id } },
         { $group: { _id: "$country", count: { $sum: 1 } } },
         { $project: { _id: 0, country: "$_id", count: 1 } },
         { $sort: { count: -1 } }
     ]);
 
     const cityStats = await CLICKS.aggregate([
+        { $match: { url: url._id } },
         { $group: { _id: "$city", count: { $sum: 1 } } },
         { $project: { _id: 0, city: "$_id", count: 1 } },
         { $sort: { count: -1 } }
     ]);
 
-     const browserStats = await CLICKS.aggregate([
+    const browserStats = await CLICKS.aggregate([
+        { $match: { url: url._id } },
         { $group: { _id: "$browser", count: { $sum: 1 } } },
         { $project: { _id: 0, browser: "$_id", count: 1 } },
         { $sort: { count: -1 } }
     ]);
 
     const deviceStats = await CLICKS.aggregate([
+        { $match: { url: url._id } },
         { $group: { _id: "$device", count: { $sum: 1 } } },
         { $project: { _id: 0, device: "$_id", count: 1 } },
         { $sort: { count: -1 } }
     ]);
 
-    // const urlCountryStats = await CLICKS.aggregate([
-    //     { $match: { url: mongoose.Types.ObjectId(urlId) } }, // filter by URL
-    //     { $group: { _id: "$country", count: { $sum: 1 } } },
-    //     { $project: { _id: 0, country: "$_id", count: 1 } },
-    //     { $sort: { count: -1 } }
-    // ]);
-console.log(countryStats,cityStats,browserStats,deviceStats);
+    console.log(countryStats, cityStats, browserStats, deviceStats);
 
-    res.render('analytics', { url,UserAllUrls, clicks,countryStats,cityStats,browserStats,deviceStats, loginStatus: req.session.isLogged });
+    res.render('analytics', { url, UserAllUrls, clicks, countryStats, cityStats, browserStats, deviceStats, loginStatus: req.session.isLogged });
 }
 module.exports = { homePage, loginPage, aboutPage, DashBoardPage, analyticsPage }
